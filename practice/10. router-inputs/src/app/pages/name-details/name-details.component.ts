17d5fdd9d88c5864330e0b617a30b26e51298b09
Dave March <dbmarch@gmail.com>
2025-12-24 08:38:06 -0500

    Use router inputs in practice 10

--- practice/10. router-inputs/src/app/pages/name-details/name-details.component.ts
+++ practice/10. router-inputs/src/app/pages/name-details/name-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-name-details',
@@ -7,12 +7,23 @@ import { Component, computed, input } from '@angular/core';
   templateUrl: './name-details.component.html',
   styleUrl: './name-details.component.scss'
 })
-export default class NameDetailsComponent {
+export default class NameDetailsComponent implements AfterViewInit {
 
   // Create a 'name' signal here, which receives the name parameter from the router
+  name = input.required<string>();
 
   // Create a computed signal 'upperCaseName' which returns the name in uppercase
+  upperCaseName = computed(() => this.name().toUpperCase());
 
   // Create a computed signal 'NameLength' which returns the length of the name
-  
+  nameLength = computed(() => this.name().length);
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+    console.log(this.name());
+  } 
+  constructor() {
+    console.log('Constructor');
+  }
 }
+
