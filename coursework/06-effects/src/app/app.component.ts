3d27bf4d3f55fb5fbe6f133a92b633614eca2c55
Dave March <dbmarch@gmail.com>
2025-12-23 20:34:43 -0500

    effects

---
+++ coursework/06-effects/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component, computed, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  }
