260fa22f8ba706c4f3b29a44d43989346f8ff738
Dave March <dbmarch@gmail.com>
2025-12-25 16:50:16 -0500

    linked signal is working

--- practice/17. books-store-with-resources/src/app/services/state-service.ts
+++ practice/17. books-store-with-resources/src/app/services/state-service.ts
@@ -1,6 +1,8 @@
-import { Injectable, resource, signal } from '@angular/core';
+import { inject, Injectable, linkedSignal, resource, signal } from '@angular/core';
 import { Book } from '../models/book';
-import { httpResource } from '@angular/common/http';
+import { httpResource, HttpClient } from '@angular/common/http';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,9 @@ export class StateService {
 
   readonly apiBase: string = 'http://localhost:3000/api/books';
   readonly wsBase: string = 'ws://localhost:3000/ws';
+  readonly http = inject(HttpClient);
+
+// Private signals
   #keyword = signal<string>('the');
 
 
@@ -18,13 +23,37 @@ export class StateService {
   //   loader: (options) => this.#searchkeywordPromise(options.params.keyword, options.abortSignal),
   //   defaultValue: []
   // })
-#searchResults = httpResource<Book[]>(() => ({
-  url: `${this.apiBase}/search`,
-  params: { q: this.#keyword() }, 
-}), {
-    defaultValue: []
-  }
-);
+  #searchResults = httpResource<Book[]>(() => ({
+    url: `${this.apiBase}/search`,
+    params: { q: this.#keyword() }, 
+  }), {
+      defaultValue: []
+    }
+  );
+
+  #selectedBookId = linkedSignal<Book[], string>({
+    source: () => this.#searchResults.value(),
+    computation:  (src, prev) => {
+      if (!prev) {
+        return src.length > 0 ? src[0].id : '';
+      }
+      if (prev.value === '' && src.length > 0) {
+        return src[0].id;
+      }
+      return prev.value;
+    }, 
+  })
+
+  #selectedBook = rxResource ({
+    params: () => {
+      console.log(this.#selectedBookId())
+      return ({ id: this.#selectedBookId() })},
+    stream: (options) => options.params.id  
+        ? this.http.get<Book>(`${this.apiBase}/${options.params.id}`) 
+        : of(null),
+    defaultValue: null
+  })
+
 
   get keyword() {
     return this.#keyword.asReadonly();
@@ -35,10 +64,23 @@ export class StateService {
     return this.#searchResults.asReadonly();
   }
 
+  get selectedBookId() {
+    return this.#selectedBookId.asReadonly();
+  }
+
+  get selectedBook() {
+    return this.#selectedBook.asReadonly();
+  }
+
   setKeyword(value: string) {
     this.#keyword.set(value);
     // console.log (this.keyword())
   }
+
+  setSelectedBookId(value:string) {
+    this.#selectedBookId.set(value);
+  }
+
 
 #searchkeywordPromise(value: string, abortSignal?: AbortSignal ): Promise<Book[]> {
   return fetch(`${this.apiBase}/search/?q=${value}`, {signal: abortSignal})
