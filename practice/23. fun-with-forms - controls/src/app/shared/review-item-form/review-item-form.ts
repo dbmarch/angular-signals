c4d406b16e9dcea0dc278383f6d5cba705808deb
Dave March <dbmarch@gmail.com>
2025-12-26 17:50:50 -0500

    fix warnings

--- practice/23. fun-with-forms - controls/src/app/shared/review-item-form/review-item-form.ts
+++ practice/23. fun-with-forms - controls/src/app/shared/review-item-form/review-item-form.ts
@@ -1,10 +1,13 @@
 import { Component, input } from '@angular/core';
-import { FieldTree } from "@angular/forms/signals";
-import { ReviewItem } from './'
+import { FieldTree, Field } from "@angular/forms/signals";
+import { ReviewItem } from '../../models/dinner-review.model'
+import { FieldWrapper } from '../field-wrapper/field-wrapper';
+import { FieldStyleDirective } from  '../field-styling.directive';
+
 @Component({
-  selector: 'app-review-item-form',
+  selector: 'app-review-form',
   standalone: true,
-  imports: [],
+  imports: [Field, FieldWrapper, FieldStyleDirective],
   templateUrl: './review-item-form.html',
   styleUrl: './review-item-form.scss',
 })
