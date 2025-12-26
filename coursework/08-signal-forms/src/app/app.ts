a477749cc9fdf304498b417b9fcb942399d57a58
Dave March <dbmarch@gmail.com>
2025-12-25 19:29:04 -0500

    signal forms - setting up the validation

---
+++ coursework/08-signal-forms/src/app/app.ts
@@ -0,0 +1,35 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DinnerReview } from './models/dinner-review.model';
+import { email, Field, form, minLength, required } from '@angular/forms/signals'
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, Field],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {
+  readonly model = signal<DinnerReview>({
+    username: '',
+    role: 'user',
+    email: '',
+    description: '',
+    rating: 1,
+  })
+
+  readonly reviewForm = form(this.model, path => {
+    required(path.username);
+    required(path.email);
+    email(path.email);
+    minLength(path.description, 5);
+  });
+
+  
+
+  constructor() {
+    // console.log (this.reviewForm);
+  }
+}
